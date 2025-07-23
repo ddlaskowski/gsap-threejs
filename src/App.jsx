0ea439e6a4cac17ba5431be06e1d2148d3726591
Damian <dd.laskowski@gmail.com>
2025-07-23 20:52:47 +0200

    Implement Navbar and logo section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import LogoSection from "./components/LogoSection";
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 
 const App = () => {
     return ( 
     <main>
+        <Navbar/>
         <Hero/>
         <ShowcaseSection/>
+        <LogoSection/>
     </main> );
 }
  
--- src/constants/index.js
+++ src/constants/index.js
@@ -12,5 +12,61 @@ const counterItems = [
   { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
-export {counterItems, words};
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+export {counterItems, words, navLinks, logoIconsList};
 
